63ca1e8904bf537d1288414bf9da1185cb3ab112
Abdullah Karaman <akaraman@ipwatch.email>
2018-10-15 23:06:09 -0500

    Adding bootstrap, improving the UI, adding more routes, adding partials

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 
 const app = express();
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
 
 const port = 5000;
 
 // Index Route
 app.get('/', (req,res) => {
-  res.send('INDEX');
+  const title = 'Hey Man';
+  res.render('index', {
+    title:title
+  });
 });
 
 // About Route
 app.get('/about', (req,res) => {
-  res.send('<h1 style="text-align: center; color: pink;">MALIKA KAKASHKA</h1><img style="text-align: center" src="https://scontent.fatl1-2.fna.fbcdn.net/v/t1.0-1/p160x160/23561511_1954106998140015_1801442717087177042_n.jpg?_nc_cat=104&oh=638c987a8b5a736599e3b2d7fbf5dcc6&oe=5C5618B2">');
+  res.render('about');
 });
 
 app.listen(port, () => {
